59951b95378bbd05787f13d5c9fb5d3947fb6aca
Masco <102709134+MascoScripts@users.noreply.github.com>
2024-03-30 13:48:40 -0400

    Update sh_ixplayermodels.lua

--- sh_ixplayermodels.lua
+++ sh_ixplayermodels.lua
@@ -1,13 +1,24 @@
+local PLUGIN = PLUGIN
+
 PLUGIN.name = "IX Player Models Fix"
 PLUGIN.author = "Masco"
 PLUGIN.description = "Fixes models in IX to make Player Models."
 
-local IXPlayerModels = {
-    "path/to/your.mdl",
-    "path/to/your.mdl"
-}
-
-for _, model in ipairs(IXPlayerModels) do
-    print("Setting " .. model .. " to a playermodel.")
-    ix.anim.SetModelClass(model, "player")
-end
+ix.anim.SetModelClass("models/player/Group01/male_01.mdl", "player")
+ix.anim.SetModelClass("models/player/Group01/male_02.mdl", "player")
+ix.anim.SetModelClass("models/player/Group01/male_03.mdl", "player")
+ix.anim.SetModelClass("models/player/Group01/male_04.mdl", "player")
+ix.anim.SetModelClass("models/player/Group01/male_05.mdl", "player")
+ix.anim.SetModelClass("models/player/Group01/male_06.mdl", "player")
+ix.anim.SetModelClass("models/player/Group01/male_07.mdl", "player")
+ix.anim.SetModelClass("models/player/Group01/male_08.mdl", "player")
+ix.anim.SetModelClass("models/player/Group01/male_09.mdl", "player")
+ix.anim.SetModelClass("models/player/Group03m/male_01.mdl", "player")
+ix.anim.SetModelClass("models/player/Group03m/male_02.mdl", "player")
+ix.anim.SetModelClass("models/player/Group03m/male_03.mdl", "player")
+ix.anim.SetModelClass("models/player/Group03m/male_04.mdl", "player")
+ix.anim.SetModelClass("models/player/Group03m/male_05.mdl", "player")
+ix.anim.SetModelClass("models/player/Group03m/male_06.mdl", "player")
+ix.anim.SetModelClass("models/player/Group03m/male_07.mdl", "player")
+ix.anim.SetModelClass("models/player/Group03m/male_08.mdl", "player")
+ix.anim.SetModelClass("models/player/Group03m/male_09.mdl", "player")
